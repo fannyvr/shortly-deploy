ad0456791301fe890f792c1575b520a6825289d1
Fanny Vasquez <fannyvr18@gmail.com>
2016-02-11 21:42:45 -0800

    Built production

--- app/config.js
+++ app/config.js
@@ -1,45 +1,22 @@
-var Bookshelf = require('bookshelf');
 var path = require('path');
+var env = require('dotenv').config({verbose: false});
+var crypto = require('crypto');
+var mongoose = require('mongoose');
+var db;
 
-var db = Bookshelf.initialize({
-  client: 'sqlite3',
-  connection: {
-    host: '127.0.0.1',
-    user: 'your_database_user',
-    password: 'password',
-    database: 'shortlydb',
-    charset: 'utf8',
-    filename: path.join(__dirname, '../db/shortly.sqlite')
-  }
+mongoUri = process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017';
+
+
+mongoose.connect(mongoUri);
+
+db = mongoose.connection;
+
+db.on('err', function(err){
+  console.log('Error connecting to database', err)
 });
 
-db.knex.schema.hasTable('urls').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('urls', function (link) {
-      link.increments('id').primary();
-      link.string('url', 255);
-      link.string('base_url', 255);
-      link.string('code', 100);
-      link.string('title', 255);
-      link.integer('visits');
-      link.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
-});
-
-db.knex.schema.hasTable('users').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
-      user.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
+db.once('open', function() {
+  console.log('Mongo is so open now!!');
 });
 
 module.exports = db;
--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,49 @@
 var db = require('../config');
+var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
-var Promise = require('bluebird');
+var bluebird = require('bluebird');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function(){
-    this.on('creating', this.hashPassword);
+var userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    index: {
+      unique: true
+    }
   },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function(){
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
+  password: {
+    type: String,
+    required: true
   }
 });
+
+var User = mongoose.model('User', userSchema);
+
+User.comparePassword = function(tried, saved, callback) {
+    bcrypt.compare(tried, saved, function(err, isMatch) {
+      if (err) {
+        console.error("An error occurred checking password: ", err);
+        callback(err);
+      }
+      callback(null, isMatch);
+    });
+  };
+  
+User.hashPassword = function(){
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.get('password'), null, null).bind(this)
+    .then(function(hash) {
+      this.set('password', hash);
+    });
+};
+  
+userSchema.pre('save', function(next) {
+  var cipher = bluebird.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+      next();
+    });
+})
 
 module.exports = User;
